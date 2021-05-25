e51ac98969f1b284b1020e51e15e17f972536dd1
mo3ist <50373064+mo3ist@users.noreply.github.com>
2021-05-25 19:35:29 +0200

    redux on landing page init

--- frontend/capsulator/src/App.js
+++ frontend/capsulator/src/App.js
@@ -2,26 +2,29 @@ import './App.css';
 import LandingPage from './components/LandingPage';
 import Navbar from './components/Navbar'
 
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 
 function App() {
   return (
-    <div className="bg-primary min-h-screen p-6">
-      {/* skeleton */}
-      <div className="md:w-5/6 m-auto h-full">
-        {/* navbar */}
-        <div className="h-20 bg-red-100">
-          <Navbar></Navbar>
-        </div>
-        {/* body */}
-        <div className="rounded-b-2xl">
+    <Router>
+      <div className="bg-primary min-h-screen p-6">
+        {/* skeleton */}
+        <div className="md:w-5/6 m-auto h-full">
+          {/* navbar */}
+          <div className="h-20 bg-red-100">
+            <Navbar></Navbar>
+          </div>
+          {/* body */}
+          <div className="rounded-b-2xl">
 
-          {/* Landing page */}
-          <LandingPage />
+            {/* Landing page */}
+            <LandingPage />
 
 
+          </div>
         </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- frontend/capsulator/src/components/Navbar.js
+++ frontend/capsulator/src/components/Navbar.js
@@ -1,20 +1,64 @@
-function Navbar() {
-    return (
-        <nav class="h-full">
-          <div class="flex overflow-hidden h-full ">
-            <div class="h-full rounded-t-2xl lg:w-80 bg-secondary flex-grow flex items-center justify-center">
-              <img src="./logo.png" alt="" className="h-full"/>
-            </div>
-            <div class="h-full lg:w-80 bg-gray-500 flex-grow flex items-center justify-center">
-              <p1 class="text-secondary text-2xl md:text-4xl font-bold">Logo</p1>
-            </div>
-            <div class="h-full lg:w-80 bg-gray-500 flex-grow flex items-center justify-center">
-              <p1 class="text-secondary text-2xl md:text-4xl font-bold">Logo</p1>
-            </div>
+import {useState} from 'react'
+import {Link} from "react-router-dom";
+import {useSelector, useDispatch} from 'react-redux'
+import classname from 'classnames'
 
+import {
+  changeTab,
+  selectActiveTab
+} from '../store/navbarSlice'
+
+function Navbar() {
+  const activeTab = useSelector(selectActiveTab)
+
+  const dispatch = useDispatch()
+
+  // Choose which page is active right now
+  const [active, setActive] = useState(0)
+
+  function changeActiveTab(tab){
+    dispatch(changeTab(tab))
+  }
+
+  return (
+      <nav class="h-full">
+        <div class="flex overflow-hidden h-full ">
+          <div class={
+            classname(
+              "h-full lg:w-80 bg-gray-500 flex-grow flex items-center justify-center rounded-t-xl", 
+              {
+                "text-secondary": activeTab !== 0,
+                "text-primary bg-secondary": activeTab === 0
+            })
+          }>
+            <Link to="/" className="h-full" onClick={() => changeActiveTab(0)}>
+              <img src="./logo.png" alt="" className="h-full"/>
+            </Link>
           </div>
-        </nav>
-    )
+          <div class={
+            classname(
+              "h-full lg:w-80 bg-gray-500 flex-grow flex items-center justify-center rounded-t-xl", 
+              {
+                "text-secondary": activeTab !== 1,
+                "text-primary bg-secondary": activeTab === 1
+            })
+          }>
+            <Link to="/create" class=" text-2xl md:text-4xl font-bold" onClick={() => changeActiveTab(1)}>Logo</Link>
+          </div>
+          <div class={
+            classname(
+              "h-full lg:w-80 bg-gray-500 flex-grow flex items-center justify-center rounded-t-xl", 
+              {
+                "text-secondary": activeTab !== 2,
+                "text-primary bg-secondary": activeTab === 2
+            })
+          }>
+            <Link to="/account" class=" text-2xl md:text-4xl font-bold" onClick={() => changeActiveTab(2)}>Logo</Link>
+          </div>
+
+        </div>
+      </nav>
+  )
 }
 
 export default Navbar
